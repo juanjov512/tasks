eb7401436ec85797ff83adb2e0d7b78a54e522a8
Juan Villegas <juanjovillegas512@outlook.com>
2025-05-06 09:26:52 -0500

    frontend
    - initial frontend configuration

---
+++ frontend/vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'tailwind-merge',
+      '@apollo/client',
+      'graphql',
+      'zustand'
+    ],
+    force: true,
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-router-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-router-dom': 'ReactRouterDOM'
+        }
+      }
+    }
+  }
+})
